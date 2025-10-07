97103671bec2fb82e5c4fd2b80e3763b1063cba3
Kayson Brown <kaysonadam28@gmail.com>
2025-10-07 15:47:05 -0600

    Finished Entry.cs

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -5,10 +5,13 @@ public class Entry
 {
     // DECLARE ATTRIBUTE
     public string _prompt;
+    public string _promptInput;
+    public string _date;
 
-    // MODIFY ATTRIBUTE
+    // PROGRAM
     public Entry()
     {
+        // MODIFY _prompt
         List<string> prompts = new List<string>()
         {
             "Who was the most interesting person I interacted with today?",
@@ -18,11 +21,23 @@ public class Entry
             "If I had one thing I could do over today, what would it be?",
             "If you could do anything you wanted right now, what would it be?"
         };
-
         Random random = new Random();
         int r = random.Next(prompts.Count);
-
         _prompt = prompts[r];
+
+        // MODIFY _date
+        DateTime theCurrentTime = DateTime.Now;
+        _date = theCurrentTime.ToShortDateString();
+    }
+
+    public void UserInput()
+    {
+        _promptInput = Console.ReadLine();
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Date: {_date} - Prompt: {_prompt}\n{_promptInput}");
     }
 }
 
@@ -37,10 +52,11 @@ Menu
 5. Quit        → End program loop
 
 ENTRY
-* User Response
-* Prompt
-* Date
-- Display ENTRY attributes
+* _promptInput
+* _prompt
+* _date
+- .UserInput --> Modifies _promptInput
+- .Display ENTRY attributes
 
 JOURNAL
 List of Entries
