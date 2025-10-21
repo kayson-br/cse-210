d33db182045a9e29f8e6e126c5d83cdaae88af3f
Kayson Brown <kaysonadam28@gmail.com>
2025-10-20 21:08:43 -0600

    Finished Core

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture scripture = new Scripture();
+        string input = "";
+        scripture.Display();
+        Console.WriteLine($"\n\nPress enter to continue or type 'quit' to finish:");
+        input = Console.ReadLine();
+
+        while (input!="quit")
+        {
+            scripture.HideWords();
+            Console.WriteLine($"\n\nPress enter to continue or type 'quit' to finish:");
+            input = Console.ReadLine();
+            if (scripture.ShowSize() == 0)
+            {
+                break;
+            }
+        }
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,105 @@
+using System;
+
+public class Scripture
+{
+    // ATTRIBUTES
+    private int _wordTotal = 0;
+    private Reference _reference = new Reference();
+    private List<List<Word>> _scripture = new List<List<Word>>();
+
+    private List<Word> _scripturePrint = new List<Word>();
+    private List<int> _scriptureShown = new List<int>();
+
+    // CONSTRUCTOR
+    public Scripture()
+    {
+        // _scripture + _wordTotal
+        List<string> scripture = new List<string>();
+        scripture = _reference.GetScripture();
+        for (int verse = 0; verse < scripture.Count(); verse++)
+        {
+            string[] verseWords = scripture[verse].Split(' ');
+            List<Word> wordList = new List<Word>();
+
+            for (int verseWord = 0; verseWord < verseWords.Count(); verseWord++)
+            {
+                Word word = new Word(verseWords[verseWord]);
+                wordList.Add(word);
+                _scripturePrint.Add(word);
+                _wordTotal++;
+            }
+            _scripture.Add(wordList);
+        }
+
+        // _scripturePrint
+        /*
+        for (int verse_i = 0; verse_i < _scripture.Count(); verse_i++)
+        {
+            List<Word> verse = new List<Word>();
+            verse = _scripture[verse_i];
+            for (int word_i = 0; word_i < verse.Count(); word_i++)
+            {
+                _scripturePrint
+            }
+        }
+        */
+
+        // _scriptureShown
+        for (int i = 0; i < _wordTotal; i++)
+        {
+            _scriptureShown.Add(i);
+        }
+    }
+
+    public void Display()
+    {
+        Console.Clear();
+        Console.Write($"\n{_reference.GetReference()}  ");
+        for (int word_i = 0; word_i < _scripturePrint.Count(); word_i++)
+        {
+            Console.Write($"{_scripturePrint[word_i].GetWord()} ");
+        }
+    }
+
+    public void HideWords()
+    {
+        Random rand = new Random();
+
+        // Random start
+        int scriptureSize = _scriptureShown.Count();
+
+        // Select loop size | Random word hide
+        if (scriptureSize > 3)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                int randomIndex = rand.Next(_scriptureShown.Count);
+                int randomStart = _scriptureShown[randomIndex];
+
+                _scriptureShown.Remove(randomStart);
+                _scripturePrint[randomStart].Hide();
+                scriptureSize--;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < scriptureSize; scriptureSize--)
+            {
+                int randomIndex = rand.Next(_scriptureShown.Count);
+                int randomStart = _scriptureShown[randomIndex];
+
+                _scriptureShown.Remove(randomStart);
+                _scripturePrint[randomStart].Hide();
+                scriptureSize--;
+            }
+        }
+
+        Display();
+    }
+    
+    public int ShowSize()
+    {
+        return _scriptureShown.Count();
+    }
+
+}
