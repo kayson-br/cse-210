7d5e3c3583f6dad3d6a2669cb8f7c677e2c3e7d8
Kayson Brown <kaysonadam28@gmail.com>
2025-11-19 14:55:36 -0700

    Initial Creation

---
+++ prove/Develop02/Goal.cs
@@ -0,0 +1,51 @@
+using System;
+
+abstract class Goal
+{
+    // ATTRIBUTES
+    private string _checkBox = "";
+    private int _points;
+    private string _name;
+    private string _description; 
+
+
+    // CONSTRUCTOR
+    public Goal(string checkBox, int points, 
+                string name, string description)
+    {
+        
+    }
+
+
+    // MODULES
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public string GetDescription()
+    {
+        return _description;
+    }
+
+    public void SetCheckBox()
+    {
+        _checkBox = "x";
+    }
+
+    public string GetCheckBox()
+    {
+        return _checkBox;
+    }
+
+    public int GetPoints()
+    {
+        return _points;
+    }
+
+
+    // ABSTRACTS
+    public abstract void SaveToFile();
+    public abstract void LoadFromFile();
+    public abstract void PrintProgress();
+}
