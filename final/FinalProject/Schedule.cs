47d4efe007b634cdc8f6dabe68b91eceb5cd220e
Kayson Brown <kaysonadam28@gmail.com>
2025-12-08 15:09:53 -0700

    Add SelectSubject function to Schedule class

--- final/FinalProject/Schedule.cs
+++ final/FinalProject/Schedule.cs
@@ -11,7 +11,42 @@ public class Schedule
     {}
 
 
-    // MODULES
+    // MODULES (Private/ Functions)
+    private Subject SelectSubject()
+    {
+            // No Subject Scenario
+        if (_subjectList.Count == 0)
+        {
+            Console.WriteLine("\nNo subjects have been added yet.");
+            return null;
+        }
+
+
+            // Display Subjects
+        Console.WriteLine("\nSelect a subject:");
+        for (int i = 0; i < _subjectList.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}.)  {_subjectList[i].GetSubjectTitle()}");
+        }
+
+
+            // Grab Subject Index
+        Console.Write("Enter choice: ");
+        string inputString = Console.ReadLine();
+        int input = int.Parse(inputString);
+        if (input >= 1 && input <= _subjectList.Count)
+        {
+            return _subjectList[input - 1];
+        }
+        else
+        {
+            Console.WriteLine("Invalid selection.");
+            return null;
+        }
+    }
+
+
+    // MODULES (Private)
     public void AddSubject()
     {
 
